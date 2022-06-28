c9962de66e4b7e0469c8c16f1615cc379558bdfb
Salman Farooq <salman.farooq3310@gmail.com>
2022-06-28 22:45:26 +0500

    modified favicon.ico

--- django_newspaper/urls.py
+++ django_newspaper/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('favicon.ico/', RedirectView.as_view(url=static('icons/news_icon_4.png'))),
+    path('favicon.ico/', RedirectView.as_view(url=static('icons/news_icon_3_white.png'))),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
