7ae1948072d1b4b10c0a4ad3ce69534d6bc36140
Salman Farooq <salman.farooq3310@gmail.com>
2022-06-29 16:20:02 +0500

    first commit

--- create_fake_data.py
+++ create_fake_data.py
@@ -1,6 +1,8 @@
 import os
+from random import choice, sample
 
 import django
+from django.contrib.auth import get_user_model
 from faker import Faker
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_newspaper.settings')
@@ -10,7 +12,24 @@ from articles.models import Article, Category, Comment
 
 fake = Faker()
 
-if True:
-    print(Article.objects.all())
-    print(Category.objects.all())
-    print(Comment.objects.all())
+for _ in range(50):
+    a = Article(
+        title=fake.sentence(nb_words=6)[:-1],
+        body=fake.text(max_nb_chars=3000),
+        author=choice(get_user_model().objects.all()),
+    )
+    a.save()
+    a.categories.add(
+        *sample(
+            list(Category.objects.all()),
+            k=4
+        )
+    )
+
+for article in Article.objects.all():
+    if article.comment_set.count() < 5:
+        for _ in range(5):
+            article.comment_set.create(
+                comment=fake.sentence(nb_words=10)[:-1],
+                author=choice(get_user_model().objects.all()),
+            )
