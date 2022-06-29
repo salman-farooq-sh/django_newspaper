e557616861f67ca62410662fb274953ebcbe127f
Salman Farooq <salman.farooq3310@gmail.com>
2022-06-29 14:13:47 +0500

    removed and replaced pages app

--- django_newspaper/settings.py
+++ django_newspaper/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
 
     'accounts.apps.AccountsConfig',
-    'pages.apps.PagesConfig',
     'articles.apps.ArticlesConfig',
 ]
 
--- django_newspaper/urls.py
+++ django_newspaper/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.templatetags.static import static
 from django.urls import path, include
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 
 urlpatterns = [
     path('favicon.ico/', RedirectView.as_view(url=static('icons/news_icon_3_white.png'))),
@@ -9,5 +9,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('articles/', include('articles.urls')),
-    path('', include('pages.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
