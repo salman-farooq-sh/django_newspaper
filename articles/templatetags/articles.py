48f62b6bb54f83540c6acee0e3ef25e2cf55512a
Salman Farooq <salman.farooq3310@gmail.com>
2022-06-30 15:52:13 +0500

    configured html to text conversion a little

--- articles/templatetags/articles.py
+++ articles/templatetags/articles.py
@@ -6,5 +6,17 @@ register = template.Library()
 
 @register.filter
 def html_to_text(html):
-    # TODO: configure this conversion
-    return html2text.html2text(html)
+    text_maker = html2text.HTML2Text()
+
+    text_maker.ignore_links = True
+    text_maker.skip_internal_links = True
+    text_maker.ignore_anchors = True
+    text_maker.ignore_emphasis = True
+    text_maker.ignore_tables = True
+    text_maker.ignore_images = True
+    text_maker.single_line_break = True
+    text_maker.emphasis = False
+    text_maker.strong_mark = ''
+    text_maker.emphasis_mark = ''
+
+    return text_maker.handle(html)
